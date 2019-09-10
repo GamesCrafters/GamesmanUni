b87736d3107a2e17223802da310716e3268d8e03
Penguinlay <13214144+Penguinlay@users.noreply.github.com>
2019-09-10 02:18:43 -0700

    change theme files naming and data

---
+++ vue/src/datas/styles/SLight.ts
@@ -0,0 +1,6 @@
+export const font = "Avenir, Helvetica";
+export const mainColor = "black";
+export const winColor = "green";
+export const drawColor = "orange";
+export const tieColor = "orange";
+export const loseColor = "red";
