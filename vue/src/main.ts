d84547d5280a2ab3883c6e1ecc64766eb802479a
Penguinlay <13214144+Penguinlay@users.noreply.github.com>
2019-09-10 02:18:57 -0700

    setup axios

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,8 +1,9 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import "@/axios";
+import "@/registerServiceWorker";
 
 Vue.config.productionTip = false;
 
