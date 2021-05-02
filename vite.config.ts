e26303d1a5fb8c9ce494898e3a8c2ec5de071481
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-05-01 17:37:43 -0700

    v5.0.0-alpha.12: Clean up old codes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-// import { resolve } from "path";
-import loadVersion from "vite-plugin-package-version";
-import vue from "@vitejs/plugin-vue";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import loadVersion from "vite-plugin-package-version";
 
 const alias: Record<string, string> = {
     "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
 };
 
-// https://vitejs.dev/config/
 export default defineConfig({
     define: { "process.env": {} },
     plugins: [
@@ -17,7 +15,6 @@ export default defineConfig({
         vueI18n({
             compositionOnly: true,
             fullInstall: false,
-            // include: resolve(__dirname, "./src/docs/**"),
         }),
     ],
     resolve: {
