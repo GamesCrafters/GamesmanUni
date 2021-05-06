f1b7196941cac4f01cf903320ec9207a6276b567
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-05-05 19:41:06 -0700

    v5.0.0-alpha.30: Reorganize how game boards are loaded and ported default regular 2D game board
    Default regular 2D game board still has a bug and haven't optimized yet but it is working for now.

--- src/components/AppGameBoard.vue
+++ src/components/AppGameBoard.vue
@@ -3,17 +3,16 @@
 </template>
 
 <script lang="ts" setup>
-    import { useRoute } from "vue-router";
-    import AppGameBoardDefault from "./AppGameBoardDefault.vue";
-    import AppGameBoardTttRegular from "./AppGameBoardTttRegular.vue";
+    import { useStore } from "../plugins/store";
+    import AppGameBoardFallback from "./AppGameBoardFallback.vue";
+    import AppGameBoardRegular2D from "./AppGameBoardRegular2D.vue";
 
-    const gameBoards: Record<string, any> = {
-        "ttt-regular": AppGameBoardTttRegular,
-    };
+    const gameBoards: Record<string, any> = {};
+    const store = useStore();
     const getGameBoardComponent = () => {
-        const route = useRoute();
-        const gameBoard = `${route.params.gameId}-${route.params.variantId}`;
+        if (store.state.app.game.round.position.match(/^R_(A|B)_([0-9]+)_([0-9]+)_([a-zA-Z0-9-\*]+)(?:_(.*))?$/)) return AppGameBoardRegular2D;
+        const gameBoard = `${store.state.app.game.id}-${store.state.app.game.variant.id}`;
         if (gameBoard in gameBoards) return gameBoards[gameBoard];
-        return AppGameBoardDefault;
+        return AppGameBoardFallback;
     };
 </script>
--- src/components/AppGameBoardFallback.vue
+++ src/components/AppGameBoardFallback.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-        <h3>Position</h3>
-        <pre id="app-game-board-default-position-value">{{ position }}</pre>
-    </div>
-    <div v-if="store.state.app.game.options.showNextMoves">
+    <h3>Position</h3>
+    <pre id="app-game-board-fallback-position">{{ position }}</pre>
+    <template v-if="store.state.app.game.options.showNextMoves">
         <hr class="c-divider" />
         <h3>Move(s)</h3>
-        <div id="app-game-board-default-moves-buttons">
+        <div id="app-game-board-fallback-available-move-buttons">
             <button v-for="availableMove in store.state.app.game.round.availableMoves" :key="availableMove.move" :class="store.state.app.game.options.showNextMoveHints ? `c-${availableMove.moveValue}` : ''" :style="{ opacity: store.state.app.game.options.showNextMoveDeltaRemotenesses ? availableMove.moveValueOpacity : 1 }" @click="store.dispatch(actionTypes.runMove, availableMove.move)">{{ availableMove.move }}</button>
         </div>
-    </div>
+    </template>
 </template>
 
 <script lang="ts" setup>
@@ -17,25 +15,17 @@
     import { actionTypes, useStore } from "../plugins/store";
 
     const store = useStore();
-
-    const position = computed(() => {
-        let position: string = store.state.app.game.round.position;
-        position = position.replace(/^;/, "");
-        position = position.replace(/;$/, "");
-        position = position.replace(/;/g, "\n");
-        position = position.replace(/=/g, " = ");
-        return position;
-    });
+    const position = computed(() => store.state.app.game.round.position.replace(/^;/, "").replace(/;$/, "").replace(/;/g, "\n").replace(/=/g, " = "));
 </script>
 
 <style lang="scss" scoped>
-    #app-game-board-default-position-value {
+    #app-game-board-fallback-position {
         border: 0.04em solid var(--neutralColor);
         border-radius: 0.25em;
         display: inline-block;
         padding: 0.5em;
     }
-    #app-game-board-default-moves-buttons {
+    #app-game-board-fallback-available-move-buttons {
         padding: 0.5em 20%;
     }
 </style>
