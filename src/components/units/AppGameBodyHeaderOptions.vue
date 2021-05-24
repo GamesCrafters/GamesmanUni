63c6106d183730fc5dfc01e5affef0ed8f5ed78c
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-05-23 19:55:45 -0700

    v5.0.0-alpha.59: fix minor UI bugs

--- src/components/units/AppGameBodyHeaderOptions.vue
+++ src/components/units/AppGameBodyHeaderOptions.vue
@@ -124,6 +124,13 @@
                             margin: 1rem;
                         }
                         > .input {
+                            border: 0.1rem solid var(--neutralColor);
+                            border-radius: 1rem;
+                            box-sizing: border-box;
+                            &:focus {
+                                box-shadow: 0.25rem 0.25rem 0.1rem 0.01rem var(--primaryColor);
+                                outline: none;
+                            }
                         }
                         > .label {
                             text-align: center;
