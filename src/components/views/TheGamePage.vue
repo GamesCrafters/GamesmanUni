e21db136bb1a18cc6d5b8b62d2ec7830b0f66224
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-05-24 05:40:13 -0700

    v5.0.0-alpha.76: Update html header page titles

--- src/components/views/TheGamePage.vue
+++ src/components/views/TheGamePage.vue
@@ -3,16 +3,16 @@
 </template>
 
 <script lang="ts" setup>
-    // import { computed } from "vue";
-    // import { useMeta } from "vue-meta";
-    // import { useRoute } from "vue-router";
-    // import { useStore } from "../../scripts/plugins/store";
+    import { computed } from "vue";
+    import { useMeta } from "vue-meta";
+    import { useRoute } from "vue-router";
+    import { useStore } from "../../scripts/plugins/store";
     import AppGame from "../units/AppGame.vue";
 
-    // const route = useRoute();
-    // const store = useStore();
-    // const game = computed(() => store.state.app.gameTypes[<string>route.params.type].games[<string>route.params.gameId]);
-    // const gameName = computed(() => game.value.name);
-    // const variantDescription = computed(() => game.value.variants.variants[<string>route.params.variantId].description);
-    // useMeta(computed(() => ({ title: `${variantDescription.value} ${gameName.value}` })));
+    const route = useRoute();
+    const store = useStore();
+    const game = computed(() => store.getters.games(route.params.type as string).games[route.params.gameId as string]);
+    const gameName = computed(() => (game.value ? game.value.name : ""));
+    const variantDescription = computed(() => (game.value && game.value.variants.variants[route.params.variantId as string] ? game.value.variants.variants[route.params.variantId as string].description : ""));
+    useMeta(computed(() => ({ title: `${variantDescription.value} ${gameName.value}` })));
 </script>
