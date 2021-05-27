90c4625313b42f1f3161663fa8e5c463397a5a42
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-05-26 17:31:32 -0700

    v5.0.0-alpha.85: Redesign the updates page

--- src/components/units/AppUpdates.vue
+++ src/components/units/AppUpdates.vue
@@ -1,38 +1,32 @@
 <template>
     <div id="app-updates">
         <h2 id="title">What's new!?</h2>
-        <div class="row">
-            <a id="description" href="javascript:wlocation.reload()">
-                If latest version listed here does not match <code>GamesmanUni</code> version in footer, new content is available.<br />
-                Please try refreshing the page <code>ctrl/command + r</code> to get latest patches and updates.
-                <!-- <br /><br /> -->
-                <!-- All dates and times are in local timezone. -->
-            </a>
-        </div>
-        <template v-if="Object.keys(latestCommits).length">
-            <a class="commit row" v-for="commit in latestCommits.commits" :key="commit.sha" :href="commit.url" target="_blank" rel="noreferrer">
-                <div class="message">
-                    <VueMarkdownIt :linkify="true" :plugins="plugins" :source="commit.message" />
+        <div id="body">
+            <template v-if="Object.keys(latestCommits).length">
+                <a id="description" href="javascript:wlocation.reload()">
+                    If latest version listed here does not match <code>GamesmanUni</code> version in footer, new content is available.<br />
+                    Please try refreshing the page <code>ctrl/command + r</code> to get latest patches and updates. <br />
+                    All dates and times are in local timezone.
+                </a>
+                <div id="commits">
+                    <a class="commit" v-for="commit in latestCommits.commits" :key="commit.sha" :href="commit.url" target="_blank" rel="noreferrer">
+                        <h3 class="commit-version">{{ commit.message.split(/:(.+)/)[0] }}</h3>
+                        <i class="message">{{ commit.message.split(/:(.+)/)[1].split(/\n(.+)/)[0] }}</i>
+                        <span class="author">
+                            <img class="avatar" :alt="commit.authorName + `'s Avatar`" :title="commit.authorName" :src="getAuthorAvatarSource(commit.authorAvatarUrl)" style="width: 3rem" />
+                            <b class="name">{{ commit.authorName }}</b>
+                        </span>
+                        <time class="datetime" :datetime="commit.date">{{ new Date(commit.date).toLocaleTimeString() }}<br />{{ new Date(commit.date).toLocaleDateString() }}</time>
+                    </a>
                 </div>
-                <p class="author">
-                    <img class="avatar" :alt="commit.authorName + `'s Avatar`" :title="commit.authorName" :src="getAuthorAvatarSource(commit.authorAvatarUrl)" style="width: 2rem" />
-                    <span class="name">{{ commit.authorName }}</span>
-                </p>
-                <!-- Temporarily disabled due to a bug in moment timezone failing the production build. -->
-                <!-- <p>{{ moment(commit.date).tz(moment.tz.guess(true)).format("LLLL") }}</p> -->
-            </a>
-        </template>
-        <div class="row" v-else>
-            <p id="failure">Failed to load GitHub commit history to this site.</p>
+            </template>
+            <p id="failure" v-else>Failed to load GitHub commit history to this site.</p>
         </div>
-        <div class="row">
-            <a id="more" href="https://github.com/GamesCrafters/GamesmanUni/commits/main" target="_blank" rel="noreferrer"> Please visit GitHub for complete update history... </a>
-        </div>
+        <a id="more" href="https://github.com/GamesCrafters/GamesmanUni/commits/main" target="_blank" rel="noreferrer"> Please visit GitHub for complete update history... </a>
     </div>
 </template>
 
 <script lang="ts" setup>
-    // import moment from "moment-timezone";
     import VueMarkdownIt from "vue3-markdown-it";
     import MarkdownItLinkAttributes from "markdown-it-link-attributes";
     import { actionTypes, useStore } from "../../scripts/plugins/store";
@@ -49,46 +43,69 @@
 
 <style lang="scss" scoped>
     #app-updates {
-        align-content: normal;
-        align-items: stretch;
-        display: flex;
-        flex-direction: column;
-        flex-wrap: nowrap;
-        height: 100%;
-        justify-content: center;
         padding: 1rem 10%;
         * {
             text-decoration: none;
             word-wrap: break-word;
         }
-        > * {
-            flex: 0 1 auto;
-            padding: 0.5rem 2rem;
-        }
         > #title {
             margin: 1rem;
         }
-        > .row {
-            border-radius: 1rem;
-            border: 0.1rem solid var(--neutralColor);
-            > .author {
-                align-content: normal;
+        #body {
+            > #description {
+                margin: 1rem;
+            }
+            > #commits {
+                align-content: center;
                 align-items: center;
                 display: flex;
                 flex-direction: row;
-                flex-wrap: nowrap;
-                justify-content: flex-start;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                > * {
-                    margin: 0 1rem;
-                    text-align: left;
-                }
-                > .avatar {
-                    border-radius: 100%;
+                flex-wrap: wrap;
+                justify-content: center;
+                > .commit {
+                    align-content: normal;
+                    align-items: center;
+                    border-radius: 1rem;
+                    border: 0.1rem solid var(--neutralColor);
+                    display: flex;
+                    flex-direction: column;
+                    flex-wrap: nowrap;
+                    justify-content: center;
+                    margin: 1rem;
+                    padding: 1rem;
+                    width: 13rem;
+                    > * {
+                        padding: 1rem 0;
+                    }
+                    > .message {
+                        font-size: 0.8rem;
+                        min-height: 3rem;
+                        line-height: 1rem;
+                        text-align: center;
+                    }
+                    > .author {
+                        align-content: normal;
+                        align-items: center;
+                        display: flex;
+                        flex-direction: column;
+                        flex-wrap: nowrap;
+                        justify-content: center;
+                        > .avatar {
+                            border-radius: 100%;
+                        }
+                    }
+                    > .datetime {
+                        font-size: 0.8rem;
+                        line-height: 1rem;
+                    }
                 }
             }
+            #failure {
+                margin: 1rem;
+            }
+        }
+        #more {
+            margin: 1rem;
         }
     }
 </style>
