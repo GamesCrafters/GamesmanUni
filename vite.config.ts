b47c0f31256ecabe437cfc3e29e4830986ecce69
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-09-08 17:50:29 -0700

    Unwind broken dependency update and undo package version dependency removal

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,19 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import legacy from "@vitejs/plugin-legacy";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-// Importing package.json exposed the whole JSON in production.
-// It should be fine for this particular project as this is an open source project.
-// I is not appropriate to do so for project where package.json should be private.
-import packageJson from "./package.json";
+import loadVersion from "vite-plugin-package-version";
 
 const alias: Record<string, string> = {
     "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
 };
 
 export default defineConfig({
-    define: { PACKAGE_VERSION: JSON.stringify(packageJson.version) },
+    define: { "process.env": {} },
     plugins: [
         legacy({
             targets: ["defaults"],
         }),
+        loadVersion(),
         vue(),
         vueI18n({
             compositionOnly: true,
