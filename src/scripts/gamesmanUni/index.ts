b1281048718eb515e2b6fd89ad735c51bb6282d5
Cameron Cheung <cameronlcheung@yahoo.com>
2022-06-10 19:29:37 -0700

    Fix computermove bug

--- src/scripts/gamesmanUni/index.ts
+++ src/scripts/gamesmanUni/index.ts
@@ -179,7 +179,7 @@ export const generateComputerMove = (round: Types.Round) => {
     const availableMoves = Object.values(round.position.availableMoves);
     const currentPositionValue = round.position.positionValue;
     let bestMoves = availableMoves.filter((availableMove) => availableMove.moveValue === currentPositionValue);
-    if (currentPositionValue === "win" || "tie") {
+    if (currentPositionValue === "win" || currentPositionValue === "tie") {
         const minimumRemoteness = Math.min(...bestMoves.map((bestMove) => bestMove.remoteness));
         bestMoves = bestMoves.filter((availableMove) => availableMove.remoteness === minimumRemoteness);
     } else if (currentPositionValue === "lose") {
