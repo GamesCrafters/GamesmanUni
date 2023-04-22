2a6d610f3e1cca3c9b3a6ffd950a44b3857d29ce
Cameron Cheung <cameronlcheung@yahoo.com>
2023-04-22 09:35:19 -0700

    v5.0.0-alpha.220: Demo Mode support Draw Games

--- src/components/units/AppDemo.vue
+++ src/components/units/AppDemo.vue
@@ -8,43 +8,54 @@
 <script lang="ts" setup>
     import { computed, ref, watch } from "vue";
     import { onBeforeRouteLeave } from "vue-router";
-    import { actionTypes, useStore } from "../../scripts/plugins/store";
+    import { actionTypes, mutationTypes, useStore } from "../../scripts/plugins/store";
     import AppDemoBody from "./AppDemoBody.vue";
     import AppGameVvh from "./AppGameVvh.vue";
 
     const store = useStore();
     const games = [
-        { gameType: "games", gameId: "1dchess", variantId: "regular" },
-        { gameType: "games", gameId: "3spot", variantId: "regular" },
-        { gameType: "games", gameId: "achi", variantId: "regular" },
-        { gameType: "games", gameId: "chomp", variantId: "4x7" },
-        { gameType: "games", gameId: "connect4c", variantId: "6x7" },
-        { gameType: "games", gameId: "dodgem", variantId: "regular" },
-        { gameType: "games", gameId: "gameofy", variantId: "dim5" },
-        { gameType: "games", gameId: "haregame", variantId: "m-hounds-first" },
-        { gameType: "games", gameId: "lite3", variantId: "three-in-a-row" },
-        { gameType: "games", gameId: "notakto", variantId: "board3" },
-        { gameType: "games", gameId: "othello", variantId: "regular" },
-        { gameType: "games", gameId: "quarto", variantId: "regular" },
-        { gameType: "games", gameId: "quickchess", variantId: "regular" },
-        { gameType: "games", gameId: "stt", variantId: "default" },
-        { gameType: "games", gameId: "snake", variantId: "regular" },
-        { gameType: "games", gameId: "tactix", variantId: "regular" },
-        { gameType: "games", gameId: "ttt", variantId: "regular" },
-        { gameType: "games", gameId: "ttt", variantId: "misere" },
-        { gameType: "games", gameId: "tttwo", variantId: "regular" },
-        { gameType: "games", gameId: "topitop", variantId: "regular" },
+        { gameType: "games", gameId: "1dchess", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "fivefieldkono", variantId: "delta", isDrawGame: true },
+        { gameType: "games", gameId: "chinesechess", variantId: "regular", isDrawGame: false, startPosition: "R_A_10_9_---k---------------------P---------------------------R-------------n------------------K---" },
+        { gameType: "games", gameId: "3spot", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "achi", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "beeline", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "chomp", variantId: "4x7", isDrawGame: false },
+        { gameType: "games", gameId: "369mm", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "connect4c", variantId: "6x7", isDrawGame: false },
+        { gameType: "games", gameId: "dinododgem", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "dodgem", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "gameofy", variantId: "dim5", isDrawGame: false },
+        { gameType: "games", gameId: "haregame", variantId: "m-hounds-first", isDrawGame: false },
+        { gameType: "games", gameId: "baghchal", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "lite3", variantId: "three-in-a-row", isDrawGame: false },
+        { gameType: "games", gameId: "Lgame", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "notakto", variantId: "board3", isDrawGame: false },
+        { gameType: "games", gameId: "othello", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "quarto", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "quickcross", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "quickchess", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "stt", variantId: "default", isDrawGame: false },
+        { gameType: "games", gameId: "dao", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "tactix", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "ttt", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "mutorere", variantId: "regular", isDrawGame: true },
+        { gameType: "games", gameId: "tttwo", variantId: "regular", isDrawGame: false },
+        { gameType: "games", gameId: "topitop", variantId: "regular", isDrawGame: false },
     ];
 
     const currentGameIndex = ref(0);
-    const initiateCurrentGame = () => {
-        store.dispatch(actionTypes.initiateMatch, {
+    const initiateCurrentGame = async () => {
+        await store.dispatch(actionTypes.initiateMatch, {
             gameType: games[currentGameIndex.value].gameType,
             gameId: games[currentGameIndex.value].gameId,
             variantId: games[currentGameIndex.value].variantId,
+            startPosition: games[currentGameIndex.value].startPosition || undefined,
             firstPlayerIsComputer: true,
             secondPlayerIsComputer: true
         });
+        store.commit(mutationTypes.setLeftPlayerIsComputer, true);
+        store.commit(mutationTypes.setRightPlayerIsComputer, true);
     }
     initiateCurrentGame();
 
@@ -52,7 +63,8 @@
     const repeat = 2;
     const currRep = ref(0);
     const gameOverTimeoutMilisec = 5000;
-    const isEndOfMatch = computed(() => store.getters.isEndOfMatch);
+    const isEndOfDrawMatch = computed(() => games[currentGameIndex.value].isDrawGame && store.getters.currentRoundId > 21);
+    const isEndOfMatch = computed(() => store.getters.isEndOfMatch || isEndOfDrawMatch.value);
     watch(
         () => isEndOfMatch.value,
         async () => {
@@ -60,6 +72,8 @@
             if (!store.getters.currentMatch.gameType) return;
             /* Prevents restarting when isEndOfMatch becomes false. */
             if (!isEndOfMatch.value) return;
+            store.commit(mutationTypes.setLeftPlayerIsComputer, false);
+            store.commit(mutationTypes.setRightPlayerIsComputer, false);
             await new Promise((resolve) => setTimeout(resolve, gameOverTimeoutMilisec));
             if (++currRep.value == repeat) {
                 /* If reached max repeat times, load the next game in list. */
