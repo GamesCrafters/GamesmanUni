cb132b9c90d498990a6ae553d68ef5abfb0d30ed
Alvaro Estrella <ajestrella@outlook.com>
2025-04-22 14:57:37 -0700

    feat(AppGameBodyHeader): Added a fast analysis toggle button to maintain button symmetry in the AppGameBodyHeader.

--- src/components/units/GameBody/AppGameBodyHeader.vue
+++ src/components/units/GameBody/AppGameBodyHeader.vue
@@ -1,9 +1,15 @@
 <template>
     <div id="app-game-body-header">
         <AppGameBodyHeaderInstructions />
-        <AppGameBodyHeaderTitle />
         <AppGameBodyHeaderOptions />
+        <AppGameBodyHeaderTitle />
         <AppGameBodyHeaderScorecard />
+        <button v-if="options.showMenu" title="Toggle Analysis"
+            @click="() => {options.showMenu = false; options.showNextMoves = false; options.showNextMoveHints = false; options.showNextMoveDeltaRemotenesses = false; options.highlightMove = false}"
+        > &lt; </button>
+        <button v-else title="Toggle Menu"
+            @click="() => {options.showMenu = true; options.showNextMoves = true; options.showNextMoveHints = true; options.showNextMoveDeltaRemotenesses = true; options.highlightMove = true}"
+        > &gt; </button>
     </div>
 </template>
 
@@ -12,7 +18,12 @@
     import AppGameBodyHeaderInstructions from "./AppGameBodyHeaderInstructions.vue";
     import AppGameBodyHeaderOptions from "./AppGameBodyHeaderOptions.vue";
     import AppGameBodyHeaderScorecard from "./AppGameBodyHeaderScorecard.vue";
+    
+    import { useStore } from "../../../scripts/plugins/store";
+    import { computed } from "vue";
 
+    const store = useStore();
+    const options = computed(() => store.getters.options);
 </script>
 
 <style lang="scss" scoped>
@@ -26,5 +37,12 @@
         > * {
             margin: 1rem;
         }
+    }
+
+    button {
+        border-radius: 100%;
+        font-size: 2rem;
+        height: max(2.5rem, min(5vh, 5vw));
+        width: max(2.5rem, min(5vh, 5vw));
     }
 </style>
